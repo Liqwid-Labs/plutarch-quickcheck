30e0567dd11eb37f247689684a71cdffab8e4a61
Seungheon Oh <seungheon.ooh@gmail.com>
2022-09-07 19:37:08 -0500

    better test

--- sample/mapzip-test/Main.hs
+++ sample/mapzip-test/Main.hs
@@ -12,11 +12,11 @@
 module Main (main) where
 
 import GHC.IO.Encoding (setLocaleEncoding, utf8)
-import Plutarch.Prelude (PBool, PBuiltinList, Term, pfilter, plam, pmap, (#), (#==), (:-->))
+import Plutarch.Prelude (PBool, PBuiltinList, PList, Term, pfilter, plam, pmap, (#), (#$), (#==), (:-->))
 import Plutarch.Test.QuickCheck (PA, fromPFun)
 import Test.Tasty (adjustOption, defaultMain, testGroup)
 import Test.Tasty.ExpectedFailure (expectFail)
-import Test.Tasty.QuickCheck (Property, QuickCheckTests, arbitrary, forAllShrink, shrink, testProperty)
+import Test.Tasty.QuickCheck (Property, QuickCheckTests, arbitrary, forAllShrink, resize, shrink, testProperty)
 
 mapzipProp :: Property
 mapzipProp = forAllShrink arbitrary shrink $ fromPFun test
@@ -32,6 +32,23 @@ mapzipProp = forAllShrink arbitrary shrink $ fromPFun test
     test = plam $ \f g x ->
       pfilter # f # (pmap # g # x) #== pmap # g # (pfilter # f # x)
 
+composeProp :: Property
+composeProp = forAllShrink arbitrary shrink $ \x ->
+  forAllShrink arbitrary shrink $ \y ->
+    forAllShrink (resize 20 arbitrary) shrink $ \z ->
+      (fromPFun test) x y z
+  where
+    test ::
+      Term
+        s
+        ( (PA :--> PA)
+            :--> (PA :--> PA)
+            :--> PList PA
+            :--> PBool
+        )
+    test = plam $ \f g x ->
+      (pmap # g #$ pmap # f # x) #== (pmap # plam (\y -> g #$ f # y) # x)
+
 main :: IO ()
 main = do
   -- This will fix some problems regarding text encoding.
@@ -40,9 +57,10 @@ main = do
     testGroup
       ""
       [ expectFail $ testProperty "map zip is equal to zip map" mapzipProp
+      , testProperty "composition should be eqaul to separate applications" composeProp
       ]
   where
     -- 100 tests is way too small for a property test to search for a counterexample,
     -- it is recommanded to use at least 10,000. However, more is better.
     go :: QuickCheckTests -> QuickCheckTests
-    go = max 10_000
+    go = max 1000
